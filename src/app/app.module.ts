e487765f5e7b17fe3bf88c5f6856a19ed3f4f775
alandreno <alan.reno@live.com>
2018-04-18 18:46:49 -0600

    Routing/prelim styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { BioComponent } from './bio/bio.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GalleryComponent,
+    BioComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'bio', component: BioComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'gallery', component: GalleryComponent },   
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
