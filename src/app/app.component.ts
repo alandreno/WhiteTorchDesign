ade7fe5279401cf5ef82529152f0f389b374c79b
alandreno <alan.reno@live.com>
2018-06-05 21:10:07 -0600

    styling w/ animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('bgImgTrigger', [
+        state('none, void', style({
+          opacity: '0'
+        })),
+        state('maximum', style({
+          opacity: '50'
+        })),
+        transition('none => maximum', animate('2000ms ease-in'))
+    ])
+]
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'app';
+  opacityState:string = 'none';
+  ngAfterViewInit() {
+    this.opacityState = 'maximum';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent },
       { path: 'bio', component: BioComponent },
